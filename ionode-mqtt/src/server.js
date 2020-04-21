37ad6c9c0ae53cdffd1a8ceb5bfeb6a7d289ba2d
Ruben Padilla <rubbenpad2@gmail.com>
2020-04-20 20:56:54 -0500

    [ADD][ionode-mqtt] mqtt-server wiht aedes

---
+++ ionode-mqtt/src/server.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const debug = require('debug')('ionode:mqtt')
+const chalk = require('chalk')
+const mqemitter = require('mqemitter-child-process')
+const redisPersistence = require('aedes-persistence-redis')
+const net = require('net')
+
+const client = mqemitter.child()
+const port = 1883
+const aedes = require('aedes')()
+
+const server = net.createServer(aedes.handle)
+
+server.listen(port, () => {
+  try {
+    console.log(chalk.cyan('[ionode-mqtt] server is running...'))
+  } catch (error) {
+    console.log(chalk.red(`[Error] ${error.message}`))
+    console.log(error.stack)
+    process.exit(1)
+  }
+})
+
+aedes.on('client', (client) => {
+  debug(`Client connected ${client ? client.id : client}`)
+})
+
+aedes.on('clientDisconnect', (client) => {
+  debug(`Client disconnected ${client ? client.id : client}`)
+})
+
+aedes.on('publish', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
